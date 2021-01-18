ab330ed6500bfcb71b35d8b8b36b2b6b82fcbbfc
dockeith <kenneth.crimson@gmail.com>
2021-01-18 13:30:50 +0100

    add loading spinner and code review

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Show loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loader
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 // Get Quote From API
 async function getQuote() {
+  loading();
   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
@@ -23,6 +39,8 @@ async function getQuote() {
       quoteText.classList.remove("long-quote");
     }
     quoteText.innerText = data.quoteText;
+    // Stop loader, show quote
+    complete();
   } catch (error) {
     //getQuote(); // repeat request if error. Disabled for now, as proxy is overloaded.
     console.log("whoops, no quote", error);
